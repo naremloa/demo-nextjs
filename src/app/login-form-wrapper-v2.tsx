d91ecce48f37757df3271cd98a5d6dfd618c4f33
naremloa <naremloa@gmail.com>
2025-06-02 00:25:35 +0800

    feat: add react-hook-form and zod

---
+++ src/app/login-form-wrapper-v2.tsx
@@ -0,0 +1,63 @@
+'use client'
+
+import type { Resolver } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+import { FormFieldWrapper, FormWrapper } from '@/components/form/form-wrapper-v2'
+import { Button } from '@/shadcn/components/ui/button'
+import { Input } from '@/shadcn/components/ui/input'
+
+const formSchema = z.object({
+  username: z.string().min(2, {
+    message: 'Username must be at least 2 characters.',
+  }),
+  password: z.string(),
+})
+
+export function LoginFormWrapper() {
+  const resolver = zodResolver(formSchema)
+  const form = useForm({
+    resolver,
+    defaultValues: {
+      username: '',
+      password: '',
+    },
+  })
+
+  const onSubmit = (data: z.infer<typeof formSchema>) => {
+    console.log('submit', data)
+  }
+
+  return (
+    <FormWrapper
+      {...form}
+      className="space-y-8"
+      onSubmit={onSubmit}
+    >
+      {control => (
+        <>
+          <FormFieldWrapper
+            control={control}
+            name="username"
+            label="Username"
+            controlRender={({ field }) => (
+              <Input placeholder="input your name" {...field} />
+            )}
+          >
+          </FormFieldWrapper>
+          <FormFieldWrapper
+            control={control}
+            name="password"
+            label="Password"
+            controlRender={({ field }) => (
+              <Input {...field} />
+            )}
+          >
+          </FormFieldWrapper>
+          <Button type="submit">Submit</Button>
+        </>
+      )}
+    </FormWrapper>
+  )
+}
---
+++ src/app/login-form.tsx
@@ -0,0 +1,64 @@
+'use client'
+import { DevTool } from '@hookform/devtools'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { Button } from '@shadcn/components/ui/button'
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@shadcn/components/ui/form'
+import { Input } from '@shadcn/components/ui/input'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+
+const formSchema = z.object({
+  username: z.string().min(2, {
+    message: 'Username must be at least 2 characters.',
+  }),
+  password: z.string(),
+})
+
+export function LoginForm() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: '',
+      password: '',
+    },
+  })
+
+  const onSubmit = (data: z.infer<typeof formSchema>) => {
+    console.log('submit', data)
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input placeholder="input your name" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Password</FormLabel>
+              <FormControl>
+                <Input {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+      {/* <DevTool control={form.control} /> */}
+    </Form>
+  )
+}
